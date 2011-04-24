f8204fe14984eec506ddfec96d20cf3cb53b156b
Henrik Frimodt Christensen <hfc@pni.dk>
2011-04-24 22:58:32 +0200

    Categories linked to items

--- app/components/items.rb
+++ app/components/items.rb
@@ -11,22 +11,36 @@ class Items < Netzke::Basepack::BorderLayoutPanel
       },{
         :region => :east,
         :title => "Details",
+        :name => "info",
         :class_name => "Netzke::Basepack::Panel",
         :width => 240,
         :split => true
-      },{
+      },:details.component(
         :region => :south,
         :title => "Items",
-        :class_name => "Netzke::Basepack::GridPanel",
-        :model => "Item",
         #:columns => [:location_id, :category__category_name, :model, :name, :serial_number],
         :height => 250,
         :split => true
-      }]
+      )]
     )
   end
 
- # Overriding initComponent
+  component :details do
+    {
+       :class_name => "Netzke::Basepack::GridPanel",
+       :model => "Item",
+
+       # do not load data initially
+       :load_inline_data => false,
+       
+       # limit the scope to the selected category (see below)
+       :scope => {:category_id => component_session[:selected_category_id]},
+       # always set category_id
+       :strong_default_attrs => {:category_id => component_session[:selected_category_id]}
+    }
+  end
+
+  # Overriding initComponent
    js_method :init_component, <<-JS
      function(){
        // calling superclass's initComponent
@@ -40,13 +54,32 @@ class Items < Netzke::Basepack::BorderLayoutPanel
  # Event handler
    js_method :on_category_selection_changed, <<-JS
      function(self, rowIndex){
-      this.selectCategory({boss_id: 100});
+      this.selectCategory({category_id: self.store.getAt(rowIndex).get('id')});
       // alert("Category id: " + self.store.getAt(rowIndex).get('id'));
      }
    JS
 
-  endpoint :select_category do |params|
-    logger.info "!!! params[:boss_id]: #{params[:boss_id]}\n"
+   endpoint :select_category do |params|
+#    logger.info "!!! params[:category_id]: #{params[:category_id]}\n"
+    component_session[:selected_category_id] = params[:category_id]
+
+    # selected category
+    category = Category.find(params[:category_id])
+  
+    details_grid = component_instance(:details)
+    details_data = details_grid.get_data
+  
+    {
+      :details => {:load_store_data => details_data, :set_title => "Items for #{category.category_name}"},
+      :info => {:update_body_html => category_info_html(category), :set_title => "#{category.category_name}"}
+    }  
+  end
+
+  def category_info_html(category)
+    res = "<h1>Category details...</h1>"
+    res << "Name: #{category.category_name}<br/>"
+    res << "More to come...."
+    res
   end
 
 end
